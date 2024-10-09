d71d19b4f56c594872d2d7b41353cb92497eeb58
Akash Vibhute <vibhuteakash123@gmail.com>
2024-10-09 11:11:29 +0530

    crm-user and its details

--- MahiAdmin/settings.py
+++ MahiAdmin/settings.py
@@ -118,17 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_URL = "/MahiAdmin/static/"
-# STATICFILES_DIRS = [
-#     BASE_DIR / "MahiAdmin/static",
-# ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATIC_URL = "/tatic/"
+STATIC_URL = "/MahiAdmin/static/"
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "MahiAdmin/static",
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATIC_URL = "/tatic/"
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
